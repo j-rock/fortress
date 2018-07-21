692a50008c0fa78e2b82c8209ea7661317ef6285
Joseph <wax818@gmail.com>
2018-07-21 01:19:36 -0500

    Checkpoint

---
+++ fortress/src/lib/world/map.rs
@@ -0,0 +1,23 @@
+use serde_json;
+
+#[derive(Deserialize)]
+struct Platform {
+    top_left_x: i32,
+    top_left_y: i32,
+    width: i32,
+    height: i32
+}
+
+#[derive(Deserialize)]
+pub struct MapConfig {
+    platforms: Vec<Platform>
+}
+
+pub struct Map {
+}
+
+impl Map {
+    pub fn new(config: MapConfig) -> Map {
+        
+    }
+}
