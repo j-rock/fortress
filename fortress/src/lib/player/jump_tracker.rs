df624aba2decadcddc737a194cab0327ffa1f47e
Joseph <wax818@gmail.com>
2018-07-30 01:10:11 -0500

    Jump is a lot better now

--- fortress/src/lib/player/jump_tracker.rs
+++ fortress/src/lib/player/jump_tracker.rs
@@ -29,7 +29,11 @@ impl JumpTracker {
         if self.jumps_left > 0 && self.current_delay == 0 {
             self.current_delay = self.delay_after_jump;
             self.jumps_left -= 1;
-            let impulse = liquidfun::box2d::common::math::Vec2::new(0.0, player_body.get_mass() * self.jump_strength);
+
+            let actual_body_velocity = *player_body.get_linear_velocity();
+            let jump_boost = self.jump_strength - actual_body_velocity.y;
+            let mass = player_body.get_mass();
+            let impulse = liquidfun::box2d::common::math::Vec2::new(0.0, mass * jump_boost);
             let body_center = *player_body.get_world_center();
             player_body.apply_linear_impulse(&impulse, &body_center, true);
         }
