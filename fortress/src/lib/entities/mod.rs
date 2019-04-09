1473773c4c8bec2a738558ad291e468337546fb1
Joseph Rock <josephwrock@gmail.com>
2019-04-08 19:50:09 -0500

    Move entity type.

--- fortress/src/lib/entities/mod.rs
+++ fortress/src/lib/entities/mod.rs
@@ -1,9 +1,9 @@
 pub mod entity;
+pub mod entity_type;
 pub mod registrar;
-pub mod types;
 
 pub use self::entity::Entity;
+pub use self::entity_type::EntityType;
 pub use self::registrar::EntityRegistrar;
 pub use self::registrar::RegisteredBody;
 pub use self::registrar::RegisteredFixture;
-pub use self::types::EntityType;
