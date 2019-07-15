732dcfb6d607b9ac98b671891025d413692d69f7
Joseph Rock <josephwrock@gmail.com>
2019-07-15 02:43:45 -0500

    Single-player camera doesnt always follow player.

--- fortress/src/lib/render/cameras/camera.rs
+++ fortress/src/lib/render/cameras/camera.rs
@@ -7,23 +7,34 @@ use crate::{
     render::CameraConfig
 };
 use glm;
-use nalgebra::Point2;
+use nalgebra::{
+    Point2,
+    Vector2,
+    Vector3,
+};
 
 pub struct Camera {
     config_manager: SimpleConfigManager<CameraConfig>,
-    position: glm::Vec3,
+    world_position: Vector3<f64>,
+    curr_half_lengths: Vector2<f64>,
 }
 
 impl Camera {
     pub fn new(config_watcher: &mut ConfigWatcher) -> StatusOr<Camera> {
         let config_manager: SimpleConfigManager<CameraConfig> = SimpleConfigManager::from_config_resource(config_watcher, "camera.conf")?;
-        let position = {
+        let (world_position, curr_half_lengths) = {
             let config =  config_manager.get();
-            glm::vec3(config.position.0, config.position.1, config.position.2)
+            let world_position = Vector3::new(
+                config.initial_position_when_no_players.0,
+                config.initial_position_when_no_players.1,
+                config.initial_position_when_no_players.2);
+            let curr_half_lengths = Vector2::new(config.physical_min_half_lengths.0, config.physical_min_half_lengths.1);
+            (world_position, curr_half_lengths)
         };
         Ok(Camera {
             config_manager,
-            position,
+            world_position,
+            curr_half_lengths,
         })
     }
 
@@ -37,7 +48,8 @@ impl Camera {
     }
 
     pub fn view(&self, lookat: glm::Vec3, up: glm::Vec3) -> glm::Mat4 {
-        glm::ext::look_at(self.position, self.position + lookat, up)
+        let position = self.position();
+        glm::ext::look_at(position, position + lookat, up)
     }
 
     pub fn position_independent_view(&self, lookat: glm::Vec3, up: glm::Vec3) -> glm::Mat4 {
@@ -53,7 +65,10 @@ impl Camera {
     }
 
     pub fn position(&self) -> glm::Vec3 {
-        self.position
+        let config = self.config_manager.get();
+        glm::vec3(self.world_position.x as f32 + config.camera_pos_offset.0,
+                  self.world_position.y as f32,
+                  -self.world_position.z as f32 + config.camera_pos_offset.1)
     }
 
     pub fn pre_update(&mut self) {
@@ -61,12 +76,69 @@ impl Camera {
     }
 
     pub fn post_update(&mut self, player_locs: Vec<Point2<f64>>) {
-        if player_locs.len() > 1 {
-            panic!("Unimplemented camera for multiplayer");
+        if player_locs.is_empty() {
+            return;
         }
-        if let Some(player_pos) = player_locs.get(0) {
-            let config = self.config_manager.get();
-            self.position = glm::vec3(player_pos.x as f32 + config.camera_pos_offset.0, self.position.y, -player_pos.y as f32 + config.camera_pos_offset.1);
+
+        let config = self.config_manager.get();
+        let mut min_coords = Vector2::new(std::f64::MAX, std::f64::MAX);
+        let mut max_coords = Vector2::new(std::f64::MIN, std::f64::MIN);
+        for player_loc in player_locs.into_iter() {
+            min_coords.x = if min_coords.x < player_loc.coords.x { min_coords.x } else { player_loc.coords.x };
+            min_coords.y = if min_coords.y < player_loc.coords.y { min_coords.y } else { player_loc.coords.y };
+            max_coords.x = if max_coords.x > player_loc.coords.x { max_coords.x } else { player_loc.coords.x };
+            max_coords.y = if max_coords.y > player_loc.coords.y { max_coords.y } else { player_loc.coords.y };
+        }
+
+        let mut teleported = false;
+        if min_coords.x < self.world_position.x - self.curr_half_lengths.x {
+            self.world_position.x = min_coords.x;
+            teleported = true;
+        }
+        if max_coords.x > self.world_position.x + self.curr_half_lengths.x {
+            self.world_position.x = max_coords.x;
+            teleported = true;
+        }
+        if min_coords.y < self.world_position.z - self.curr_half_lengths.y {
+            self.world_position.z = min_coords.y;
+            teleported = true;
+        }
+        if max_coords.y > self.world_position.z + self.curr_half_lengths.y {
+            self.world_position.z = max_coords.y;
+            teleported = true;
+        }
+        if teleported {
+            return;
+        }
+
+        let x_no_move_half_length = config.physical_no_move_ratios.0 * self.curr_half_lengths.x;
+        let x_min_move = min_coords.x < self.world_position.x - x_no_move_half_length;
+        let x_max_move = max_coords.x > self.world_position.x + x_no_move_half_length;
+        if x_min_move && x_max_move {
+            println!("Expand x");
+        } else if x_min_move {
+            let small_window_x = self.world_position.x - x_no_move_half_length;
+            let large_window_x = self.world_position.x - self.curr_half_lengths.x;
+            self.world_position.x -= (small_window_x - min_coords.x) / (min_coords.x - large_window_x) * config.physical_max_move_speed;
+        } else if x_max_move {
+            let small_window_x = self.world_position.x + x_no_move_half_length;
+            let large_window_x = self.world_position.x + self.curr_half_lengths.x;
+            self.world_position.x += (max_coords.x - small_window_x) / (large_window_x - max_coords.x) * config.physical_max_move_speed;
+        }
+
+        let z_no_move_half_length = config.physical_no_move_ratios.1 * self.curr_half_lengths.y;
+        let z_min_move = min_coords.y < self.world_position.z - z_no_move_half_length;
+        let z_max_move = max_coords.y > self.world_position.z + z_no_move_half_length;
+        if z_min_move && z_max_move {
+            println!("Expand z");
+        } else if z_min_move {
+            let small_window_z = self.world_position.z - z_no_move_half_length;
+            let large_window_z = self.world_position.z - self.curr_half_lengths.y;
+            self.world_position.z -= (small_window_z - min_coords.y) / (min_coords.y - large_window_z) * config.physical_max_move_speed;
+        } else if z_max_move {
+            let small_window_z = self.world_position.z + z_no_move_half_length;
+            let large_window_z = self.world_position.z + self.curr_half_lengths.y;
+            self.world_position.z += (max_coords.y - small_window_z) / (large_window_z - max_coords.y) * config.physical_max_move_speed;
         }
     }
 
--- fortress/src/lib/render/cameras/camera_config.rs
+++ fortress/src/lib/render/cameras/camera_config.rs
@@ -5,7 +5,12 @@ pub struct CameraConfig {
     pub z_far: f32,
     pub lookat: (f32, f32, f32),
     pub right: (f32, f32, f32),
-    pub position: (f32, f32, f32),
     pub camera_pos_offset: (f32, f32),
+    pub initial_position_when_no_players: (f64, f64, f64),
+
+    pub physical_min_half_lengths: (f64, f64),
+    pub physical_max_half_lengths: (f64, f64),
+    pub physical_no_move_ratios: (f64, f64),
+    pub physical_max_move_speed: f64
 }
 
