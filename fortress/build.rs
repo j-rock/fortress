e0cc30e671de952dd41db8be9b7f4ee8de605acc
Joseph Rock <josephwrock@gmail.com>
2020-04-01 19:48:49 -0500

    Copy all fonts into build out dir.

--- fortress/build.rs
+++ fortress/build.rs
@@ -11,10 +11,24 @@ use fortress_bake::{
 use std::path::PathBuf;
 
 fn copy_font(input_output: &InputOutput) -> StatusOr<()> {
-    let font = "veger_regular.ttf";
-    let font_in = input_output.input.fonts.join(font);
-    let font_out = input_output.output.fonts.join(font);
-    file::util::copy_file(font_in, font_out)
+    let directory_iterator = input_output.input.fonts.read_dir()
+        .map_err(|e| format!("Bad font dir: {:?}", e))?
+        .filter_map(|entry| entry.ok())
+        .map(|entry| entry.file_name());
+
+    for base_name in directory_iterator {
+        if let Some(base_name_str) = base_name.to_str() {
+            if !base_name_str.ends_with(".ttf") {
+                continue;
+            }
+
+            let input = input_output.input.fonts.join(&base_name);
+            let output = input_output.output.fonts.join(&base_name);
+            file::util::copy_file(input, output)?;
+        }
+    }
+
+    Ok(())
 }
 
 fn main() -> StatusOr<()> {
