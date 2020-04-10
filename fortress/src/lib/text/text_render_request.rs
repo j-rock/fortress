1412012edcb1db4fd3963b866a0971e2c44a91fa
Joseph Rock <josephwrock@gmail.com>
2020-04-10 03:21:15 -0500

    Update text render request docs.

--- fortress/src/lib/text/text_render_request.rs
+++ fortress/src/lib/text/text_render_request.rs
@@ -12,9 +12,9 @@ pub enum TextContent {
 pub struct TextRenderRequest {
     pub content: TextContent,
 
-    // Specifies top-left corner of first character.
+    // Specifies bottom-left corner of first character.
     // X in [0.0, 1.0) which goes from left to right.
-    // Y in [0.0, 1.0) which goes from top to bottom.
+    // Y in [0.0, 1.0) which goes from bottom to top.
     // Z in [0.0, 1.0) where 0.0 is closest to camera.
     pub screen_position_percentage: glm::Vec3,
 
