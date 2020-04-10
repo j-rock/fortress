79f8d9741aa650eb2bd779adb7f084a1fd823b30
Joseph Rock <josephwrock@gmail.com>
2020-04-10 03:20:59 -0500

    Update test text data.

--- fortress/src/lib/world/world_state.rs
+++ fortress/src/lib/world/world_state.rs
@@ -195,8 +195,8 @@ impl WorldState {
         {
             self.text_renderer.queue(TextRenderRequest {
                 content: TextContent::Text(NamedText::GameInAlpha),
-                screen_position_percentage: glm::vec3(0.0, 0.0, 0.1),
-                raster_scale_multiplier: 1.0,
+                screen_position_percentage: glm::vec3(0.0, 0.0, 0.0),
+                raster_scale_multiplier: 2.3,
                 raster_size: RasterSize::Large,
                 color: glm::vec3(0.0, 1.0, 0.0),
                 alpha: 1.0,
